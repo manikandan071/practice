e5ee883e0c1fa81b1f7ddc3f8c11df2307ebde53
manikandan <manidhiyamech07167@gmail.com>
2022-08-16 16:53:03 +0530

    js-math

---
+++ js-math/js/main.js
@@ -0,0 +1,61 @@
+
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+console.log(Math.SQRT1_2);
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.LOG2E);
+console.log(Math.LOG10E);
+
+
+var result = Math.max(10,7,30,33,66,70,88);
+console.log("maxvalue :",result);
+
+var result = Math.min(10,7,30,33,66,70,88);
+console.log("minvalue :",result);
+
+var result1 = Math.round(2.49);
+console.log('round :',result1);
+
+var result2 = Math.round(2.5);
+console.log('round :',result2);
+
+var result3 = Math.ceil(21.1);
+console.log('ceil :',result3);
+
+var result4 = Math.ceil(-21.1);
+console.log('ceil :',result4);
+
+var result5 = Math.floor(1.1);
+console.log('floor :',result5);
+
+var result6 = Math.floor(-1.1);
+console.log('floor :',result6);
+
+var result7 = Math.trunc(-61.1);
+console.log('trunc :',result7);
+
+var result8 = Math.sign(21.1);
+console.log('sign :',result8);
+
+var result9 = Math.pow(4,2);
+console.log('sign :',result9);
+
+var result9 = Math.sqrt(64);
+console.log('sign :',result9);
+
+var result10 = Math.abs(-21.1);
+console.log('sign :',result10);
+
+console.log("The sine value of 90 degrees is " + Math.sin(90 * Math.PI / 180));
+
+console.log("The cosine value of 60 degrees is " + Math.cos(60 * Math.PI / 180));
+
+console.log(Math.random());
+
+console.log(Math.log(8));
+
+console.log(Math.log2(8));
+
+console.log(Math.log10(1000));
